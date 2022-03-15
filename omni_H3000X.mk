33c29329c3412fa2fa3c7de237a111fd71ba3d9b
ycly2333 <56163845+ycly2333@users.noreply.github.com>
2022-03-15 19:32:33 +0800

    Update omni_H3000X.mk

--- omni_H3000X.mk
+++ omni_H3000X.mk
@@ -4,7 +4,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from H3000X device
-$(call inherit-product, device/eebbk/H3000X/device.mk)
+$(call inherit-product, device/EEBBK/H3000X/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
@@ -15,4 +15,4 @@ PRODUCT_DEVICE := H3000X
 PRODUCT_NAME := omni_H3000X
 PRODUCT_BRAND := EEBBK
 PRODUCT_MODEL := S3 Pros
-PRODUCT_MANUFACTURER := eebbk
+PRODUCT_MANUFACTURER := EEBBK
